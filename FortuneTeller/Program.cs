be67c664d552fcd318cc5eb1b4f618fd7f9fe99a
wenjen10 <wenjen10@yahoo.com>
2019-05-14 16:17:55 -0400

    first commit

---
+++ FortuneTeller/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FortuneTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Fortune Teller");
+
+        }
+    }
+}
