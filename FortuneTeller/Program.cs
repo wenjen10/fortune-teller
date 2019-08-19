05ed328a646cca272db4c40d4bbd9fdf4f4f2177
wenjen10 <wenjen10@yahoo.com>
2019-08-19 16:07:14 -0400

    pushing many changes

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -4,40 +4,134 @@ namespace FortuneTeller
 {
     class Program
     {
-        //private static bool month;
-        //private static string cout;
+        private static int birthMonth;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Fortune Teller");
 
             Console.WriteLine("What is your first name?");
-            
+
             string firstName = Console.ReadLine();
 
             Console.WriteLine("Hello," + firstName);
-            Console.WriteLine("What is your last name?");
+
             Console.WriteLine("What is your age?");
+            //if age is odd they retire in 1 year else if age is even they retire in 2 years
 
-            //ask user for their birth month from 1-12
-            Console.WriteLine("What is your birth month?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            string retirementYears;
 
-            //save birth month in a variable of type string
-            string usersBirthMonth = Console.ReadLine();
-            string bankBalance = "";
+            if (age % 2 == 0)
+            {
+                retirementYears = "2 years";
+            }else{
+                retirementYears = "1 year";
+            }
 
-            //if birth month = "1" then bank account = $300,000
-            if (usersBirthMonth == "1")
+            Console.WriteLine($"your age is {age}");
+
+            string bankBalance;
+            if (birthMonth >= 1 && birthMonth <= 4)
             {
                 bankBalance = "$300,000";
             }
+            else if (birthMonth >= 5 && birthMonth <= 8)
+            {
+                bankBalance = "$3,000,000";
+            }
+            else if (birthMonth >= 9 && birthMonth <= 12)
+            {
+                bankBalance = "$300,000,000";
+            }
 
-            //if birth month = "2" then bank account = $300,000
-            //...
-            //
+            string whatColor;
+            string transportation;
+            do
+            {
+                Console.WriteLine("what is your favorite ROYGBIV color? if you're unsure type \'help\'");
+                whatColor = Console.ReadLine().ToLower();
 
-            Console.WriteLine(firstName + " will retire with " + bankBalance +  " in the bank.");
+                switch (whatColor)
+                {
+                    case "red":
+                        transportation = "racecar";
+                        break;
+                }
+                
+              
+                switch (whatColor)
+                {
+                    case "orange":
+                        transportation = "oxen";
+                        break;
+                }
+                switch (whatColor)
+                {
+                    case "yellow":
+                        transportation = "yacht";
+                        break;
+                }
+                switch (whatColor)
+                {
+                    case "green":
+                        transportation = "glider";
+                        break;
+                }
+                switch (whatColor)
+                {
+                    case "blue":
+                        transportation = "blimp";
+                        break;
+
+                }
+                switch (whatColor)
+                {
+                    case "indigo":
+                        transportation = "inflatable";
+                        break;
+
+                }
+                switch (whatColor)
+
+                {
+                    case "violet":
+                        transportation = "van";
+                        break;
+
+                }
+
+                Console.WriteLine("How many siblings do you have?");
+                //if number of siblings is
+            int siblings = Convert.ToInt32(Console.ReadLine());
+                string numberofsiblings;
+
+                if (siblings % == 1)
+
+                {
+                    numberofsiblings = "1 sibling";
+
+                }
+                else { }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                Console.WriteLine($"{firstName} will retire with " + bankBalance +  " in the bank.");
+
+            Console.ReadLine();
 
         }
     }
 }
+            
